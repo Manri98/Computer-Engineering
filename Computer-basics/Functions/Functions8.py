d41d759f0a8f1aaf25f548ef2f06108eac134dc7
Carlos Manrique Enguita <Manri98@users.noreply.github.com>
2016-11-10 10:08:23 +0100

    Create Functions8.py

---
+++ Computer-basics/Functions/Functions8.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        Functions8.py
+#
+# Author:      uo258425
+#
+# Created:     10/11/2016
+# Copyright:   (c) uo258425 2016
+#-------------------------------------------------------------------------------
+
+
+from math import sqrt, pi
+
+
+point = [0, 0]
+
+
+def getDistanceToOrigin(point):
+    return sqrt(point[0]**2+point[1]**2)
+
+
+def getPoint():
+    point[0] = int(raw_input("Type the X coordinate: "))
+    point[1] = int(raw_input("Type the Y coordinate: "))
+    return point
+
+
+print pi*getDistanceToOrigin(getPoint())**2
