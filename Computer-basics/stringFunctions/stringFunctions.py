c484b65e64296b2333803131c44cf1a103b2a5e9
Carlos Manrique Enguita <Manri98@users.noreply.github.com>
2016-11-24 10:58:07 +0100

    .

--- Computer-basics/stringFunctions/stringFunctions.py
+++ Computer-basics/stringFunctions/stringFunctions.py
@@ -8,6 +8,14 @@
 #-------------------------------------------------------------------------------
 
 def greatestIndex(myList):
+    """ Returns the index of the biggest number
+    param:
+        myList (list)
+    return:
+        -1 (int) if the list is empty.
+        bigger (int) the biggest index otherwise
+
+    """
     if len(myList)==0:
         return -1
     bigger = 0
@@ -15,6 +23,7 @@ def greatestIndex(myList):
         if myList[i]>myList[bigger]:
             bigger = i
     return bigger
+
 
 def sum(myList):
     if len(myList)==0:
@@ -46,47 +55,50 @@ def rotateLeft(myList, step):
         j += step
     return myList
 
+print makeTags("p", "this is a test")
+print rotateLeft("hello", -1)
+lista=[-1,-2,30,4,523,6,7,8,9]
+print greatestIndex(lista)
+print sum(lista)
+print writeZeros(lista)
+
+
 
 
 def popularName(myList):
-    for i in range(1, len(myList)-1, 2):
-        if myList[i]>myList[i+2]:
-            bigger = i
-    return myList[i-1]
-
+    bigger = myList[1]
+    j = 1
+    for i in range(1, len(myList), 2):
+        if myList[i]>bigger:
+            bigger = myList[i]
+            j = i
+    return myList[j-1]
 
 list1 = ["ana", 223, "laura", 204, "elena", 175]
 list2 = ["laura", 204, "ana", 223, "elena", 175]
 list3 = ["elena", 175, "laura", 204, "ana", 223]
-
 print popularName(list1)
 print popularName(list2)
 print popularName(list3)
 
-def rot13(myText):
-    if len(myText)==0:
-        return -1
-    rotedText = ""
-    for i in range(len(myText)):
-        #lowecase
-        if 96<ord(myText[i])123:
-            rotedText += chr(ord(myText[i])+13%26+967)
-        #Uppercase
-        elif 64<ord(myText[i])<91:
-            rotedText += chr(ord(myText[i])+13%26+65)
-        #Symbols or numbers
-        else:
-            rotedText += myText[i]
-    return rotedText
 
-print rot13("This is a test")
-print rot13(rot13("This is a test"))
+##def rot13(myText):
+##    if len(myText)==0:
+##        return -1
+##    rotedText = ""
+##    for i in range(len(myText)):
+##        #lowecase
+##        if 96<ord(myText[i])<123:
+##            rotedText += chr(ord(myText[i])+13%26)
+##        #Uppercase
+##        elif 64<ord(myText[i])<91:
+##            rotedText += chr(ord(myText[i])+13%26)
+##        #Symbols or numbers
+##        else:
+##            rotedText += myText[i]
+##    return rotedText
+##
+##print rot13("This is a test")
+##print rot13(rot13("This is a test"))
+##
 
-print makeTags("p", "this is a test")
-print rotateLeft("hello", -1)
-
-lista=[-1,-2,30,4,523,6,7,8,9]
-
-print greatestIndex(lista)
-print sum(lista)
-print writeZeros(lista)
